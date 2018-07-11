e47f05a32b398ec2d433608b323bec11fff8693b
kosatun@aol.com <kosatun@aol.com>
2018-07-11 23:51:52 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,10 +13,8 @@ def input_to_index(user_input)
 end  
 
 def valid_move?(board, index)
-  if index.between?(0, 8)
-    return true
-    
-  elsif board[index] == "X" || board[index] == "O"
+  
+  if board[index] == "X" || board[index] == "O"
     return false  
   
   elsif board[index] == " " || board[index] == ""
